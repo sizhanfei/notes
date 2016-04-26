5074a5d6ad723347f15648e8e3cdae622b465c45
sizhanfei <67781757@qq.com>
2016-04-26 22:16:20 +0800

    猜拳游戏（运用随机数）

--- game_caiquan.java
+++ game_caiquan.java
@@ -74,3 +74,72 @@ public class caiquan {
 		
 
 }
+
+package test1;
+
+import java.util.Scanner;
+
+public class caiquantwo {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("请输入一个数字（1是石头2是剪刀3是布）：");
+		int shuzi=input.nextInt();
+		int suijishu=(int) (Math.random()*3+1);
+		if(shuzi==1){
+			switch(suijishu){
+			case 1:
+				System.out.println("平局！");
+				break;
+			case 2:
+				System.out.println("胜利！");
+				break;
+			case 3:
+				System.out.println("失败！");
+				break;
+				default:
+					System.out.println("错误！");
+			}}
+		else{
+			if(shuzi==2)
+			{
+				switch(suijishu){
+				case 1:
+					System.out.println("失败！");
+					break;
+				case 2:
+					System.out.println("平局！");
+					break;
+				case 3:
+					System.out.println("胜利！");
+					break;
+					default:
+						System.out.println("错误！");
+				}
+			}
+			else{
+				if(shuzi==3)
+				{
+					switch(suijishu){
+					case 1:
+						System.out.println("胜利！");
+						break;
+					case 2:
+						System.out.println("失败！");
+						break;
+					case 3:
+						System.out.println("平局！");
+						break;
+						default:
+							System.out.println("错误！");
+					}
+				}
+			}
+				
+		}
+		
+		
+		}
+
+}
+
+
